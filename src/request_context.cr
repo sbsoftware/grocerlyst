96e3462e7ece0ef37b909c75f3ef73afe18dd02f
Stefan Bilharz <stefan@sbsoftware.de>
2024-07-16 20:40:28 +0200

    EKL-16 Increase session lifetime

--- src/request_context.cr
+++ src/request_context.cr
@@ -1,6 +1,10 @@
 class Crumble::Server::RequestContext
-  def session_store
+  def self.init_session_store
     FileSessionStore.new("tmp/sessions")
+  end
+
+  def session_cookie_max_age
+    365.days
   end
 
   def list_policy
