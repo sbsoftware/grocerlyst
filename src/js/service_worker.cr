68a51e83429df1ae0a89a88e91decbfc4eecd1dc
Stefan Bilharz <stefan@sbsoftware.de>
2024-08-02 15:46:53 +0200

    EKL-24 Cache font files in service worker

--- src/js/service_worker.cr
+++ src/js/service_worker.cr
@@ -6,6 +6,33 @@ class ServiceWorker < JS::Code
   end
 
   def_to_js do
-    console.debug("Service Worker installed")
+    cacheName = "einkaufslisteApp"
+    cachedResources = [] of String
+    cachedResources.push(RobotoRegular.uri_path.to_js_ref)
+    cachedResources.push(MaterialIconFont.uri_path.to_js_ref)
+
+    self.addEventListener("install") do |event|
+      event.waitUntil(
+        caches.open(cacheName).then do |cache|
+          return cache.addAll(cachedResources)
+        end
+      )
+    end
+
+    self.addEventListener("fetch") do |event|
+      if cachedResources.includes(URL.new(event.request.url).pathname)
+        event.respondWith(
+          caches.match(event.request).then do |cachedResponse|
+            if cachedResponse
+              return cachedResponse
+            else
+              fetch(event.request).then do |response|
+                return response
+              end
+            end
+          end
+        )
+      end
+    end
   end
 end
