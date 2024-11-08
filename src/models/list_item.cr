d8f7316b18e87a5e3e5e7e17191449984128a051
Stefan Bilharz <stefan@sbsoftware.de>
2024-11-08 17:16:26 +0100

    EKL-29 Remove old monkeypatch

--- src/models/list_item.cr
+++ src/models/list_item.cr
@@ -1,13 +1,5 @@
 require "../actions/delete_record_action"
 
-struct NamedTuple
-  def to_html_attrs(_tag, attr_hash)
-    each do |key, value|
-      attr_hash[key.to_s] = value
-    end
-  end
-end
-
 class ListItem < Orma::Record
   id_column id : Int32?
   column name : String?
