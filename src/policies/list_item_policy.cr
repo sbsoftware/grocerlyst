33bd99307bcde0aab94fdc0e0be893cd99b1e4d3
Stefan Bilharz <stefan@sbsoftware.de>
2024-12-30 21:19:53 +0100

    EKL-48 Allow list item deletion for non-owners

--- src/policies/list_item_policy.cr
+++ src/policies/list_item_policy.cr
@@ -4,6 +4,16 @@ class ListItemPolicy
   def initialize(@ctx); end
 
   def delete?(list_item)
+    owns_list?(list_item) || list_access_permission?(list_item)
+  end
+
+  private def owns_list?(list_item)
     list_item.list.session_id == ctx.session.id.to_s
+  end
+
+  private def list_access_permission?(list_item)
+    list_item.list.list_access_permissions.any? do |list_access_permission|
+      list_access_permission.session_id == ctx.session.id.to_s
+    end
   end
 end
