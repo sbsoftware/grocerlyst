dbb2bd812335a9c7c9a1590c323661697cf98e8c
Stefan Bilharz <stefan@sbsoftware.de>
2025-03-07 17:17:17 +0100

    EKL-50 Allow sharing for all participants of a list

--- src/policies/list_policy.cr
+++ src/policies/list_policy.cr
@@ -4,15 +4,11 @@ class ListPolicy
   def initialize(@ctx); end
 
   def show?(list)
-    return true if owns?(list)
-
-    list.list_access_permissions.to_a.any? do |list_access_permission|
-      list_access_permission.session_id == ctx.session.id.to_s
-    end
+    owns?(list) || has_access?(list)
   end
 
   def share?(list)
-    owns?(list)
+    owns?(list) || has_access?(list)
   end
 
   def accessible_lists
@@ -29,5 +25,11 @@ class ListPolicy
 
   private def owns?(list)
     list.session_id == ctx.session.id.to_s
+  end
+
+  private def has_access?(list)
+    list.list_access_permissions.to_a.any? do |list_access_permission|
+      list_access_permission.session_id == ctx.session.id.to_s
+    end
   end
 end
