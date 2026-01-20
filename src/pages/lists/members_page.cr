e8e9c2e6453a71b1b62ea46fe3c6ab325471db05
Stefan Bilharz <stefan@sbsoftware.de>
2026-01-20 21:42:59 +0100

    EKL-64 Inline list members view template in the page

--- src/pages/lists/members_page.cr
+++ src/pages/lists/members_page.cr
@@ -1,6 +1,6 @@
 require "../application_page"
-require "../../views/lists/members_view"
 require "../../models/list"
+require "../../resources/list_resource"
 
 module Lists
   class MembersPage < ApplicationPage
@@ -14,8 +14,52 @@ module Lists
       end
     end
 
-    def page_view
-      Lists::MembersView.new(ctx, List.find(id))
+    view do
+      class BackLink
+        getter list : List
+
+        def initialize(@list); end
+
+        ToHtml.instance_template do
+          a href: ListResource.uri_path(list.id) do
+            Crumble::Material::Icon.new("arrow_back")
+          end
+        end
+      end
+
+      css_class Member
+
+      template do
+        Crumble::Material::TopAppBar.new(
+          leading_icon: BackLink.new(list),
+          headline: "Members",
+          trailing_icons: [] of Nil,
+          type: :center_aligned
+        )
+        div do
+          list.list_access_permissions.each do |list_access_permission|
+            Crumble::Material::ListItem.to_html do
+              div Member do
+                Crumble::Material::Icon.new("account_circle")
+                if name = list_access_permission.name
+                  name
+                else
+                  i do
+                    "Anonymous"
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      style do
+        rule Member do
+          display :flex
+          gap 8.px
+        end
+      end
     end
 
     def window_title : String?
