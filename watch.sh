4dbb20324fcf99275299cc9560122a4415ab6c3a
Stefan Bilharz <stefan@sbsoftware.de>
2026-03-19 18:55:41 +0100

    Move ENV vars out of the repo

--- watch.sh
+++ watch.sh
@@ -1,11 +1,7 @@
 #/bin/sh
 
-export DATABASE_URL=sqlite3://./data.db
-export ORMA_CONTINUOUS_MIGRATION=1
-export OTEL_SERVICE_NAME=EKL-dev
-export OTEL_TRACES_EXPORTER=http
-export OTEL_EXPORTER_OTLP_ENDPOINT=https://otlp.eu01.nr-data.net:4318
-export OTEL_EXPORTER_OTLP_HEADERS=\"api-key=eu01xx2b05d29d985321e8dfaaa8f970FFFFNRAL\"
-export LOG_LEVEL=trace
+set -a
+source ./.env
+set +a
 
 lib/crumble/src/watch.sh einkaufsliste 3002
